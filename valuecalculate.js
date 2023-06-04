b5b54e30ac047ae7c4660bf20ad8b978514123e1
Aditya Chandra <chandraaditya300@gmail.com>
2023-06-04 20:46:01 +0700

    Create valuecalculate.js

---
+++ valuecalculate.js
@@ -0,0 +1,9 @@
+var angka = 10;
+
+function mundur(){
+    document.getElementById("angka").innerHTML = angka=angka-1;
+}
+
+function maju(){
+    document.getElementById("angka").innerHTML = angka=angka+1;
+}
